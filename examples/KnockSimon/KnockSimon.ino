9d73f4b45b96f9bc5733ea3cadcc895145ac1410
Anderson Antunes <anderson.utf@gmail.com>
2017-11-29 11:58:17 -0200

    Replace speaker

--- examples/KnockSimon/KnockSimon.ino
+++ examples/KnockSimon/KnockSimon.ino
@@ -19,12 +19,42 @@
  *
  */
 
+/*
+ * INSTRUCTIONS:
+ *
+ * [!]:knock [_]:wait [~]:vibration
+ * [S]:simon [U]:player
+ *
+ * U: *pull cord*
+ * S: <ON> !
+ * options:
+ *   U:!) Start game
+ *     S: !!
+ *     U: !!
+ *     [sequence increases by one random pause between knocks]
+ *     ...
+ *     S: !!_!... (random sequence)
+ *     U: !_!!... (wrong sequence)
+ *     S: ~ (game over)
+ *     S: *knock once for each level*
+ *     S: (if highscore) ~_~
+ *   U:!!) Highscore
+ *     S: *knock once for each level*
+ *   U:!!!) Turn Off
+ *   U:!!!!) Reverse Game
+ *   U:!!!_!) Enable God Mode
+ *
+ *
+ */
+
+
+
 #include "Knock.h"
 
 #define RED_LED    13
-#define GREEN_LED  4
-#define SPEAKER    5
-
+#define GREEN_LED  5
+#define ACTR_S     5
+#define ACTR_V     5
 
 void fGodMode();
 void fPlayHighScore();
@@ -36,29 +66,22 @@ public:
   void init() {
     pinMode(RED_LED, OUTPUT);
     pinMode(GREEN_LED, OUTPUT);
-    pinMode(SPEAKER, OUTPUT);
+    pinMode(ACTR_V, OUTPUT);
+    pinMode(ACTR_S, OUTPUT);
 
     randomSeed(analogRead(A1));
 
     registerSequence(&game);
     registerSequence(&highScoreSeq);
+    registerSequence(&cheat);
 
     resetGame();
-
-    if (digitalRead(2)) {
-      registerSequence(&cheat);
-      digitalWrite(RED_LED, HIGH);
-      delay(50);
-      digitalWrite(RED_LED, LOW);
-      expect(3, 0, 1, 2);
-      playSequence(cheat);
-      delay(200);
-    }
   }
 
 protected:
+
   void onKnock() {
-    tone(SPEAKER, 329, 100);
+    knockBack(100);
     digitalWrite(GREEN_LED, HIGH);
     delay(debounceDelay);
     digitalWrite(GREEN_LED, LOW);
@@ -73,18 +96,20 @@ protected:
   }
 
   void onSequenceNotFound() {
-    tone(SPEAKER, 329, 300);
-    delay(300);
-    tone(SPEAKER, 161, 300);
-    delay(300);
-
     digitalWrite(RED_LED, HIGH);
     delay(50);
     digitalWrite(RED_LED, LOW);
 
-    if (!godMode) {
+    if (reverseGame) {
+      delay(500);
+      playSequence(game);
+      playSequence(*getInputSequence());
+      expectOnly(1);
+    } else if (!godMode) {
+      knockBack(100);
       resetGame();
-    }  else {
+    } else {
+      vibrate(200);
       delay(500);
       playSequence(game);
       expect(3, 0, 1, 2);
@@ -92,25 +117,19 @@ protected:
   }
 
 public:
-  void playSequence(Sequence seq) {
-    int i;
-    tone(SPEAKER, 529, 100);
-    for (i = 0; i < seq.length && seq.sequence[i] > 0; i++) {
-      delay(seq.sequence[i] * 2);
-      noTone(SPEAKER);
-      delay(50);
-      tone(SPEAKER, 529, 100);
-    }
+
+  void knockBack(int t){
+    digitalWrite(ACTR_S, HIGH);
+    delay(50);
+    digitalWrite(ACTR_S, LOW);
+    delay(t);
   }
 
-  void nextLevel () {
-    game.sequence[game.length] = random(1, 5) * 50;
-    game.length++;
-    if (game.length > highScore) {
-      highScore = game.length;
-    }
-    playSequence(game);
-    expectOnly(0);
+  void vibrate(int t){
+    digitalWrite(ACTR_V, HIGH);
+    delay(t);
+    digitalWrite(ACTR_V, LOW);
+    delay(50);
   }
 
   void resetGame() {
@@ -124,28 +143,51 @@ public:
     expect(2, 0, 1);
   }
 
+  void playSequence(Sequence seq) {
+    int i;
+    knockBack(0);
+    for (i = 0; i < seq.length && seq.sequence[i] > 0; i++) {
+      delay(seq.sequence[i] * 2);
+      delay(50);
+      knockBack(0);
+    }
+  }
+
+  void nextLevel () {
+    game.sequence[game.length] = random(1, 5) * 50;
+    game.length++;
+    if (game.length > highScore) {
+      highScore = game.length;
+    }
+    playSequence(game);
+    expectOnly(0);
+  }
+
   void playHighScore() {
     int i;
     for (i = 0; i < highScore; i++) {
-      tone(SPEAKER, 529, 50);
-      delay(50);
-      tone(SPEAKER, 829, 100);
+      knockBack(0);
       delay(300);
     }
-    tone(SPEAKER, 229, 100);
+    vibrate(200);
     delay(200);
+    vibrate(200);
     resetGame();
   }
 
   void toggleGodMode() {
     godMode = !godMode;
-    tone(SPEAKER, 900, 200);
+    vibrate(100);
+    knockBack(0);
+    vibrate(100);
+    knockBack(0);
     delay(200);
   }
 
 private:
   uint8_t highScore = 0;
   bool godMode = false;
+  bool reverseGame = false;
   Sequence game = (Sequence) {
     .sequence = new uint8_t[MAX_KNOCKS], .length = 0, .action = fNextLevel, .enabled = false
   };
